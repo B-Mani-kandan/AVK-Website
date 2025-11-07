02368cfb1b074b3c04a00bf78e45614dab8d9484
B-Mani-kandan <baskarmanikandan48@gmail.com>
2025-11-07 11:28:27 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,6 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import NavBar from "./Components/NavBar/NavBar";
-import Home from "./Components/Home/Home";
-import Footer from "./Components/Footer/Footer";
-import ContactUs from "./Components/ContactUs/ContactUs";
-import AboutUs from "./Components/AboutUs/AboutUs";
-import SeaFreight from "./Components/ServicePages/SeaFreight/SeaFreight";
-import AirFreight from "./Components/ServicePages/AirFreight/AirFreight";
-import HeavyLift from "./Components/ServicePages/HeavyProject/HeavyLift";
-import ScrollToTop from "./ScrollToTop";
-import { useEffect } from "react";
-import AOS from "aos";
-import "aos/dist/aos.css";
-function App() {
-  useEffect(() => {
-    AOS.init({
-      duration: 1000,
-      once: true,
-      startEvent: "DOMContentLoaded",
-    });
-
-    setTimeout(() => {
-      AOS.refreshHard();
-    }, 100);
-  }, []);
-  return (
-    <>
-      <Router>
-        <NavBar />
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Contact-Us" element={<ContactUs />} />
-          <Route path="/About-Us" element={<AboutUs />} />
-          <Route path="/Ocean-Freight-Services" element={<SeaFreight />} />
-          <Route path="/Air-Freight-Services" element={<AirFreight />} />
-          <Route path="/Heavy-Lift-Cargo" element={<HeavyLift />} />
-          {/* <Route path="/Road-Freight-Services" element={<Transportation />} />
-        
-        <Route path="/Air-Freight-Services" element={<AirFreight />} />
-        <Route path="/FTWZ-Operations" element={<FTWZ />} />
-        <Route path="/Customs-Clearance" element={<CustomClearence />} /> */}
-        </Routes>
-        {/* {location.pathname !== "/Contact-Us" && <HomeContact />} */}
-        {/* <HomeDesc /> */}
-        <Footer />
-      </Router>
-    </>
-  );
-}
+import "./index.css";
+const App = () => {
+  return <div>Welcome to Invoking Systems Private Limited </div>;
+};
 
 export default App;
